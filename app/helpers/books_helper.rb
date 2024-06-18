e71268f03956647e37899aea403d2acbbfb85fda
yuichi.imori <yuichi.imori@classi.jp>
2024-06-18 10:51:46 +0900

    change: helperに登録していたメソッドを削除、view内での記載に変更
    helper.new, helper.edit のような登録をやめ、view.common.new, view.common.editのような方法で登録した

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,23 +1,4 @@
 # frozen_string_literal: true
 
 module BooksHelper
-  def show_text
-    t 'helpers.show'
-  end
-
-  def new_text
-    t 'helpers.new'
-  end
-
-  def edit_text
-    t 'helpers.edit'
-  end
-
-  def back_text
-    t 'helpers.back'
-  end
-
-  def delete_text
-    t 'helpers.delete'
-  end
 end
