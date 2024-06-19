0ca58672ca43d62685a4e75b629ca506ad0a4272
yuichi.imori <yuichi.imori@classi.jp>
2024-06-19 16:05:13 +0900

    delete: userフォルダのviewを削除したので、routes.rbも併せて修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :books
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
