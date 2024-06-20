2ed11a5b48deb49617611a9b2eef3d7193d42ebb
yuichi.imori <yuichi.imori@classi.jp>
2024-06-20 15:55:30 +0900

    define: アップロードするファイル形式を制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+
+  validates :avatar, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'] }
 end
