6581512a0a99f33c7b88c593ec75d5b238fecea4
yuichi.imori <yuichi.imori@classi.jp>
2024-06-21 15:39:25 +0900

    add: comments_controller.rbを編集し、コメントの投稿機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
+  @comment = @commentable.comments.build(comment_params)
+  @comment.user = current_user
+
+  redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) if @comment.save
+
+  def set_commentable
+    if params[:book_id]
+      @commentable = Book.find(params[:book_id])
+    elsif params[:report_id]
+      @commentable = Report.find(params[:report_id])
+    end
+  end
+
+  def set_comment
+    @comment = @commentable.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
