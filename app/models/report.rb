54ef645a2086a732a9606e3d543657e61a67f518
yuichi.imori <yuichi.imori@classi.jp>
2024-06-25 16:40:56 +0900

    add: 日報が保存された時にURLがあれば中間テーブルに保存するメソッドを追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -13,11 +13,27 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  after_save :save_mentions
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  private
+
+  def save_mentions
+    mentioning_relationships.destroy_all
+
+    url = %r{http://localhost:3000/reports/(\d+)}
+    ids = content.to_s.scan(url).flatten.uniq
+    reports = Report.where(id: ids).where.not(id:)
+
+    reports.each do |report|
+      MentionReport.create(mentioning_report: self, mentioned_report: report)
+    end
   end
 end
