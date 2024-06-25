72d71d9cb550bf7645630537b97b21165937c613
yuichi.imori <yuichi.imori@classi.jp>
2024-06-25 16:39:12 +0900

    add: 中間テーブルの関係をmodel内で定義

--- app/models/mention_report.rb
+++ app/models/mention_report.rb
@@ -1,4 +1,6 @@
 class MentionReport < ApplicationRecord
-  belongs_to :mentioned_report
-  belongs_to :mentioning_report
+  belongs_to :mentioned_report, class_name: 'Report'
+  belongs_to :mentioning_report, class_name: 'Report'
+
+  validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
 end
