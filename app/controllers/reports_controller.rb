7ab2b80c1f7cbe05392d2c812112b2203c219f45
yuichi.imori <yuichi.imori@classi.jp>
2024-07-04 16:56:52 +0900

    delete: コメントのjsonファイルの関する記述を削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,12 +3,12 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
 
-  # GET /reports or /reports.json
+  # GET /reports
   def index
     @reports = Report.includes(:user).order(id: :desc).page(params[:page])
   end
 
-  # GET /reports/1 or /reports/1.json
+  # GET /reports/1
   def show
     @comment = Comment.new
     @comments = @report.comments.order(id: :desc)
@@ -23,7 +23,7 @@ class ReportsController < ApplicationController
   # GET /reports/1/edit
   def edit; end
 
-  # POST /reports or /reports.json
+  # POST /reports
   def create
     @report = current_user.reports.build(report_params)
 
@@ -34,7 +34,7 @@ class ReportsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
+  # PATCH/PUT /reports/1
   def update
     if @report.update(report_params)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
